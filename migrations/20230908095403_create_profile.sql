66acd50550828aec94264964fe8d08c5943f7aa5
Agung Yuliyanto <agung.96tm@gmail.com>
2023-09-08 10:33:13 +0700

    adjustment migration files and format

--- migrations/20230908095403_create_profile.sql
+++ migrations/20230908095403_create_profile.sql
@@ -10,9 +10,11 @@ CREATE TABLE "profile" (
     deleted_at TIMESTAMP NULL
 );
 ALTER TABLE "profile" ADD CONSTRAINT "profile_user_id_fk_user_id" FOREIGN KEY ("user_id") REFERENCES "user" ("id") DEFERRABLE INITIALLY DEFERRED;
+COMMIT;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE "profile" CASCADE;
+COMMIT;
 -- +goose StatementEnd
