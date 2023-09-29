b0cb7c36858f1b80f1abdaf8470a414f549b529a
Agung Yuliyanto <agung.96tm@gmail.com>
2023-09-29 18:31:21 +0700

    Swagger: fix path and doc in config + adjustment router

--- api/controllers/main_controller.go
+++ api/controllers/main_controller.go
@@ -1,17 +1,21 @@
 package controllers
 
 import (
+	"github.com/BetterToPractice/go-echo-setup/lib"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 type MainController struct {
+	config lib.Config
 }
 
-func NewMainController() MainController {
-	return MainController{}
+func NewMainController(config lib.Config) MainController {
+	return MainController{
+		config: config,
+	}
 }
 
 func (c MainController) Index(ctx echo.Context) error {
-	return ctx.Redirect(http.StatusFound, "/swagger/index.html")
+	return ctx.Redirect(http.StatusFound, c.config.Swagger.DocUrl)
 }
